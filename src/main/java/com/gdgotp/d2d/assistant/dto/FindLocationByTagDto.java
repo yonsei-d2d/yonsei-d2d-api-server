3cfb768713a47891f057557528013defc8b76dce
Myeonghoon Lee <11175606+mhl6829@users.noreply.github.com>
2024-12-03 02:26:20 +0900

    [add] New tags

--- src/main/java/com/gdgotp/d2d/assistant/dto/FindLocationByTagDto.java
+++ src/main/java/com/gdgotp/d2d/assistant/dto/FindLocationByTagDto.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonClassDescription;
 import lombok.Data;
 
 @Data
-@JsonClassDescription("You can find location ID by tag. Tag must be on of the following lise [photo, restaurant, cafeteria, store, cafe, convenience, bank, museum, flower, postoffice, stroll]")
+@JsonClassDescription("You can find location ID by tag. Tag must be on of the following lise [photo, restaurant, cafeteria, souvenir, stationery, cafe, convenience, bank, museum, flower, postoffice, stroll]")
 public class FindLocationByTagDto {
     private String tag;
 }
---
+++ src/main/resources/db/migration/V11__updateTag.sql
@@ -0,0 +1,3 @@
+UPDATE "public"."tag" SET "tag" = 'souvenir' WHERE "location_id" = 'ed95f583-eee0-52c8-a908-7cfe75eca6f9';
+UPDATE "public"."tag" SET "tag" = 'stationery' WHERE "location_id" = '461ad820-c5ae-5630-a2e0-40e63cd99820';
+INSERT INTO "public"."tag" ("id", "tag", "location_id") VALUES (DEFAULT, 'stationery', 'ed95f583-eee0-52c8-a908-7cfe75eca6f9');
