80f51ff832896ca0fd82a2840c86d6d5d74b811a
Myeonghoon Lee <11175606+mhl6829@users.noreply.github.com>
2024-12-03 03:05:09 +0900

    [add] New alias

---
+++ src/main/resources/db/migration/V12__addNewAlias.sql
@@ -0,0 +1,6 @@
+INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (87, '백주년기념관 박물관', '0bb96044-64c1-5d88-87f1-ebb50ccc1bca');
+INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (88, '우체국', '0cec7b58-75e8-5ff2-b517-91988ae54d5b');
+INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (89, '런어스 스튜디오', '110194e6-d9f2-5f97-9d5c-58d62a3bf6f1');
+INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (90, '꽃누리샘', '5f9cd717-4206-5e6c-aa52-c28b073febca');
+INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (91, '세븐일레븐 연세대과학관점 (편의점)', '6c52c162-9225-50e0-8d68-8a71a7dcb70f');
+INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (92, '우리은행', '774327af-7d05-5aa7-b7bd-bf974e35b41e');
