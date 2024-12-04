433a08fa1ba4e11925639f48d400fac360397758
Myeonghoon Lee <11175606+mhl6829@users.noreply.github.com>
2024-12-05 03:57:34 +0900

    [fix] Alias Id typo

--- src/main/resources/db/migration/V13__addNewLocation2.sql
+++ src/main/resources/db/migration/V13__addNewLocation2.sql
@@ -6,4 +6,4 @@ UPDATE location SET geom = ST_SetSRID(ST_MakePoint(longitude, latitude), 4326);
 
 INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (93, '연세대학교 남문', 'f7be2888-58c7-569f-9fdf-1203e4641f5d');
 INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (94, '연세대학교 정문', '1d9c35d2-4703-5bf1-9438-b12ee77ad05e');
-INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (95, '연세대학교 서문', '1d9c35d2-4703-5bf1-9438-b12ee77ad05e');
+INSERT INTO "public"."alias" ("id", "name", "location_id") VALUES (95, '연세대학교 서문', '9304ae55-7a9a-562d-815f-0bdfed3c1479');
